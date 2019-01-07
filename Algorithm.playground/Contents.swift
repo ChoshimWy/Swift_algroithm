6257d1b0bed1247d63edf53ee11a6a0aa0103e06
Choshim丶Wy <824041965@qq.com>
2019-01-07 17:15:04 +0800

    commit

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -14,11 +14,11 @@ func linearSearch<T: Equatable>(array: [T], _ object: T) -> Int? {
 }
 
 let array = [5, 3, 4, 2, 7, 8]
-let index = linearSearch(array: array, 1)
-print(index)
+linearSearch(array: array, 4)
+
 
 /**
  性能:
- 1.最差情况下O(n),
- 2.最佳情况下O(1),
+ 1.最差情况下O(n),运行时需要将所查找的对象和数组中的每个对象进行比较,因此所花费的时间和数组的长度成正比.
+ 2.最佳情况下O(1),运行时如果数组的第一个元素就是所需要查找的对象,那么理想情况下所f花费的时间是最短的.
  */
